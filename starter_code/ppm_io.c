40aa993d614ef3223710e8406c19d396b4f966da
Brandon Tang <you@somewhere.com>
2020-03-31 00:25:02 -0400

    implemented ppm file read

--- starter_code/ppm_io.c
+++ starter_code/ppm_io.c
@@ -11,14 +11,43 @@
 
 /* Read a PPM-formatted image from a file (assumes fp != NULL).
  * Returns the address of the heap-allocated Image struct it
- * creates and populates with the Image data.
+ * creates and populates with the Image data. Returns NULL if 
+ * there is an issue with the file.
  */
 Image * read_ppm(FILE *fp) {
 
   // check that fp is not NULL
   assert(fp); 
 
-  return NULL;  //TO DO: replace this stub
+  Image *img;
+  Pixel *pix;
+  //char header[18];
+
+  if (fgetc(fp) != 'P' || fgetc(fp) != '6') {
+    return NULL;
+  }
+
+  // not sure how important the whitespace fgetc is
+  fgetc(fp); // guaranteed whitespace
+  fscanf(fp, "%d", &img->cols);
+  fgetc(fp); // guaranteed whitespace
+  fscanf(fp, "&d", &img->rows);
+  fgetc(fp); // guaranteed whitespace
+  
+  unsigned char color_check = 0;
+  fscanf(fp, "%d", &color_check);
+  if (color_check != 255) {
+    return NULL;
+  }
+
+  fgetc(fp); //guaranteed whitespace
+
+  pix = malloc(rows * cols * sizeof(Pixel));
+  img->data = malloc(sizeof(pix) * sizeof(Pixel));  
+  fread(pix, sizeof(Pixel), sizeof(pix), fp);
+  fwrite(img->data, sizeof(Pixel), sizeof(pix), pix);
+  
+  return img;  //TO DO: replace this stub
   
 }
 
