4560d5b6e669b98b9062bbb6ac123b9ec8f74b0f
Brandon Tang <you@somewhere.com>
2020-04-01 04:00:59 -0400

    add some helpful comments

--- project.c
+++ project.c
@@ -1,1 +1,16 @@
 //project.c
+//#include some shit
+
+int main (int argc, char *argv[]) {
+
+  // argc is the number of command-line arguments
+  // argv contains the arguments
+  // for example:
+  //  ./project trees.ppm trees-exp-half.ppm exposure 0.5
+  //  argc is 4
+  //  argv[0] is "trees.ppm"
+  //  argv[1] is "trees-exp..."
+  //  argv[2] is "exposure"
+  //  argv[3] is "0.5"
+  
+}
